f40a8a6672327731bf35ce79ff1b93e10debad26
ChiaChen <s124202468@gmail.com>
2017-07-11 08:31:19 +0800

    Refine ui and config main

--- app/src/main/java/com/example/chiachen/loginpractice/MainActivity.java
+++ app/src/main/java/com/example/chiachen/loginpractice/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.chiachen.loginpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.util.List;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+	@BindViews({R.id.button, R.id.button1})
+	List<View> mBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+	}
+
+	@OnClick({R.id.info_token, R.id.info_list})
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.info_token: {
+				Log.e(MainActivity.class.getSimpleName(), "info_token");
+				break;
+			}
+
+			case R.id.info_list: {
+				Log.e(MainActivity.class.getSimpleName(), "info_list");
+				break;
+			}
+		}
 	}
 }
